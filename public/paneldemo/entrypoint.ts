a644d5a50f80d24ca1ad23011b886aa49f5263f4
James Kafader <jkafader@es.net>
2024-04-14 23:47:57 -0700

    flesh out commentary

--- public/paneldemo/entrypoint.ts
+++ public/paneldemo/entrypoint.ts
@@ -37,6 +37,19 @@ const GrafContext = function(dashboardUid) {
         resolve(GrafContext._appContext);
       })
     }
+    // To Grafana Devs: With all of this code below, I get the feeling that
+    // "there's an easier way" to do all of this that I'm not privvy to.
+    // I looked over SoloPanelPage in particular to try my hand at a rewrite
+    // and ended up right back at square one -- bootstrapping the entire application
+    // from first principles. Do you guys have a better way of doing this stuff?
+    // I sure don't after 2 serious tries at it.
+    // 
+    // NB: how to these 'setBackendSrv' etc things work? After looking at them
+    // they're a singleton pattern that writes into the in-memory module namespace.
+    // a nice pattern! Nicer still if a lot of these were self-configured in the 
+    // module namespace with an override function rather than relying on 
+    // bootstrapping to instantiate.
+    // 
     // cribbed from /public/app/app.ts#L130
     setBackendSrv(backendSrv);
     // cribbed from /public/app/app.ts#145
@@ -52,12 +65,15 @@ const GrafContext = function(dashboardUid) {
     // cribbed from /public/app/app.ts#232-3. very likely this is nonsense we want to override/mock
     const newAssetsChecker = new NewFrontendAssetsChecker();
     newAssetsChecker.start();
+    // more cribs form /public/app/app.ts
     const dashboardSrv = new DashboardSrv();
     setDashboardSrv(dashboardSrv);
-    locationService.push("/"); // force url fetcher not to append weird random dashboard state to initial URL
+    // force API URL fetcher not to append weird random dashboard state junk to initial URL
+    locationService.push("/"); 
     // config is "magically" bound to window.grafanaBootData.
     // we need some way to get window.grafanaBootData as a module,
-    // or API call or something
+    // or API call or something. API Call has best security feels.
+    // most likely to be approved by sec/legal
     console.log('config is ', config);
     // cribbed from /public/app/app.ts#L245
     GrafContext._appContext =  {
@@ -69,6 +85,9 @@ const GrafContext = function(dashboardUid) {
         newAssetsChecker,
         config,
     };
+    // this is _absolutely_ necessary. After a big rewrite/reorg of all of this
+    // code we definitely need to do the initDashboard shenanigans in here to 
+    // get this working at all.
     let initDashboardParams = {
       fixUrl: false,
       keybindingSrv: keybindingService,
@@ -77,11 +96,18 @@ const GrafContext = function(dashboardUid) {
       urlSlug: "",
       urlType: "",
     };
+    // the line below is a significant cleanup and improvement of my understanding
+    // of what's intended to happen here. we're intented to use the redux store
+    // to "dispatch" :eyeroll: this async wart. React. Go figure.
     return GrafContext._appContext.store.dispatch(initDashboard(initDashboardParams)).then(()=>{
       console.log("we have now initted the dashboard... presumably. Returning appContext");
       return GrafContext._appContext;
     });
 };
+// I'm pretty sure this getState thing is unnecessary and doesn't do anything
+// at all currently. Particularly because it's broken at the moment. 
+// (GrafContext._stateStore is always undefined).
+// I think this is another (early) attempt to force a singleton pattern here.
 GrafContext.getState = function(dashboardUid) {
     "use strict";
     if(GrafContext._stateStore){
@@ -101,6 +127,8 @@ let bindToElementId = function(appContext, dashboardUid, panelId, elementId, hei
   })
 }
 
+// this should very likely be changed back over to the createRoot syntax
+// but it's getting late and I'm tired
 let bindToElement = function(appContext, dashboardUid, panelId, element, height, width){
   getPanel(appContext, dashboardUid, panelId, height, width).then(function(panelElem){
     console.log("rendering", panelElem, "under", element, "with ReactDOM.render");
@@ -113,6 +141,9 @@ let bindToElement = function(appContext, dashboardUid, panelId, element, height,
   });
 }
 
+// dashCache and panelCache are here to overcome situations (that I observed in the wild)
+// where the panel can have differing stateKeys from render to render. It's very likely
+// that the grafana devs have a much better mechanism for this than this loose rig.
 const dashCache = {}
 const panelCache = {}
 
@@ -139,8 +170,14 @@ let getPanel = function(appContext, dashboardUid, panelId, height, width){
       panel = panelCache[`${dashboardUid}-${panelId}`];
     }
     console.log("getPanel. after getDashboardByUid. dash, panel:", dash, panel);
+    // this Provider call binds the store in a way that the rest of the app can find it
     return React.createElement(Provider, { store: appContext.store },
+      // this one provides a lot of other state informationn and is consistent with
+      // the current bootstrapping stuff in app.ts
       React.createElement(GrafanaContext.Provider, appContext,
+          // boom. here we actually create our dashboard panel
+          // "lazy" is a very important value here. We probably always
+          // want this set up like this in an external panel context.
           React.createElement(DashboardPanel, {
             stateKey: panel.key,
             width: width,
