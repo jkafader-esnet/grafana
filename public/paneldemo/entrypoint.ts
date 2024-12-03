df8cebb031a39a16d4b5517971ecf8fbf3d71f48
James Kafader <jkafader@es.net>
2024-12-03 09:55:38 -0800

    changes to async resolve the global settings object

--- public/paneldemo/entrypoint.ts
+++ public/paneldemo/entrypoint.ts
@@ -6,8 +6,9 @@ import ReactDOM from 'react-dom';
 import { registerEchoBackend, setEchoSrv, setQueryRunnerFactory, locationService, navigationLogger, setDataSourceSrv, getBackendSrv, setBackendSrv, setLocationSrv } from '@grafana/runtime';
 import { setPanelRenderer } from '@grafana/runtime/src/components/PanelRenderer';
 import { ErrorBoundaryAlert, GlobalStyles, ModalRoot, ModalsProvider } from '@grafana/ui';
-import config from 'app/core/config';
+import { GrafanaBootConfig } from '@grafana/runtime/src/config';
 import { profiler } from 'app/core/profiler';
+import { config } from 'app/core/config';
 import { GrafanaContext } from 'app/core/context/GrafanaContext';
 import { KeybindingSrv } from 'app/core/services/keybindingSrv';
 import { AppChromeService } from 'app/core/components/AppChrome/AppChromeService';
@@ -30,12 +31,45 @@ import { setVariableQueryRunner, VariableQueryRunner } from '../app/features/var
 import { configureStore } from '../app/store/configureStore.ts';
 
 
+// in this context, we'll be getting the config asynchronously.
+// Normally, window.grafanaBootData is set in index.html and
+// the 'config' system relies upon this synchronicity. Here,
+// we'll manually instantiate it instead.
+function getConfig(){
+
+  function checkAndResolveLater(resolve, reject, timeout){
+    if(timeout > 3000){ reject('Application did not load in time!') }
+    let bootData = (window as any).grafanaBootData;
+    if(!!bootData){
+      const options = bootData.settings;
+      options.bootData = bootData;
+
+      const conf = new GrafanaBootConfig(options);
+      resolve(conf);
+    } else {
+      window.setTimeout(()=>{
+        checkAndResolveLater(resolve, reject, timeout + 100)
+      }, timeout);
+    }
+  }
+
+  return new Promise((resolve, reject)=>{
+    checkAndResolveLater(resolve, reject, 100);
+  })
+
+}
+
 const GrafContext = function(dashboardUid) {
+  return getConfig().then((conf)=>{
     if(GrafContext._appContext){
       return new Promise((resolve)=>{
-        console.log("we have already initted the application. Returning appContext early.");
         resolve(GrafContext._appContext);
       })
+    }
+    // monkeypatch the synchronous config. There appears to be no 'easy' way around this,
+    // not sure if it will work.
+    for(let key in conf){
+      config[key] = conf[key];
     }
     // To Grafana Devs: With all of this code below, I get the feeling that
     // "there's an easier way" to do all of this that I'm not privvy to.
@@ -43,20 +77,20 @@ const GrafContext = function(dashboardUid) {
     // and ended up right back at square one -- bootstrapping the entire application
     // from first principles. Do you guys have a better way of doing this stuff?
     // I sure don't after 2 serious tries at it.
-    // 
+    //
     // NB: how to these 'setBackendSrv' etc things work? After looking at them
     // they're a singleton pattern that writes into the in-memory module namespace.
-    // a nice pattern! Nicer still if a lot of these were self-configured in the 
-    // module namespace with an override function rather than relying on 
+    // a nice pattern! Nicer still if a lot of these were self-configured in the
+    // module namespace with an override function rather than relying on
     // bootstrapping to instantiate.
-    // 
+    //
     // cribbed from /public/app/app.ts#L130
     setBackendSrv(backendSrv);
     // cribbed from /public/app/app.ts#145
     setLocationSrv(locationService);
     // cribbed from /public/app/app.ts#203
     const dataSourceSrv = new DatasourceSrv();
-    dataSourceSrv.init(config.datasources, config.defaultDatasource);
+    dataSourceSrv.init(conf.datasources, conf.defaultDatasource);
     setDataSourceSrv(dataSourceSrv);
     // cribbed from /public/app/app.ts#230
     const chromeService = new AppChromeService();
@@ -69,24 +103,21 @@ const GrafContext = function(dashboardUid) {
     const dashboardSrv = new DashboardSrv();
     setDashboardSrv(dashboardSrv);
     // force API URL fetcher not to append weird random dashboard state junk to initial URL
-    locationService.push("/"); 
-    // config is "magically" bound to window.grafanaBootData.
-    // we need some way to get window.grafanaBootData as a module,
-    // or API call or something. API Call has best security feels.
-    // most likely to be approved by sec/legal
-    console.log('config is ', config);
+    locationService.push("/");
+    // API Call has best security feels. Most likely to be approved by sec/legal
+    const store = configureStore();
     // cribbed from /public/app/app.ts#L245
     GrafContext._appContext =  {
         backend: backendSrv,
         location: locationService,
         chrome: chromeService,
         keybindings: keybindingService,
-        store: configureStore(), // this is not used by GrafanaContext.Provider but is used by Provider to boostrap redux stuff
+        store: store,
         newAssetsChecker,
-        config,
+        conf,
     };
     // this is _absolutely_ necessary. After a big rewrite/reorg of all of this
-    // code we definitely need to do the initDashboard shenanigans in here to 
+    // code we definitely need to do the initDashboard shenanigans in here to
     // get this working at all.
     let initDashboardParams = {
       fixUrl: false,
@@ -100,12 +131,12 @@ const GrafContext = function(dashboardUid) {
     // of what's intended to happen here. we're intented to use the redux store
     // to "dispatch" :eyeroll: this async wart. React. Go figure.
     return GrafContext._appContext.store.dispatch(initDashboard(initDashboardParams)).then(()=>{
-      console.log("we have now initted the dashboard... presumably. Returning appContext");
       return GrafContext._appContext;
     });
+  });
 };
 // I'm pretty sure this getState thing is unnecessary and doesn't do anything
-// at all currently. Particularly because it's broken at the moment. 
+// at all currently. Particularly because it's broken at the moment.
 // (GrafContext._stateStore is always undefined).
 // I think this is another (early) attempt to force a singleton pattern here.
 GrafContext.getState = function(dashboardUid) {
@@ -131,13 +162,7 @@ let bindToElementId = function(appContext, dashboardUid, panelId, elementId, hei
 // but it's getting late and I'm tired
 let bindToElement = function(appContext, dashboardUid, panelId, element, height, width){
   getPanel(appContext, dashboardUid, panelId, height, width).then(function(panelElem){
-    console.log("rendering", panelElem, "under", element, "with ReactDOM.render");
     ReactDOM.render(panelElem, element);
-    /*console.log("panelElem", panelElem, "element", element);
-    let root = createRoot(element);
-    console.log("root is", root);
-    root.render(panelElem);
-    console.log("completed render");*/
   });
 }
 
@@ -148,28 +173,22 @@ const dashCache = {}
 const panelCache = {}
 
 let getPanel = function(appContext, dashboardUid, panelId, height, width){
-  console.log("getPanel. appContext, dashboardUid, panelId, height, width", appContext, dashboardUid, panelId, height, width);
   return backendSrv.getDashboardByUid(dashboardUid).then(function(data){
-    console.log("getPanel. after getDashboardByUid. data:", data);
     let dash = null;
     if(!dashCache[`${dashboardUid}`]){
-      console.log("fresh dashboard. caching");
       dash = new DashboardModel(data.dashboard);
       dashCache[`${dashboardUid}`] = dash;
     } else {
-      console.log("serving dashboard from cache");
       dash = dashCache[`${dashboardUid}`];
     }
     let panel = null;
     if(!panelCache[`${dashboardUid}-${panelId}`]){
-      console.log("fresh panel. caching");
       let panel = dash.getPanelById(panelId);
       panelCache[`${dashboardUid}-${panelId}`] = panel;
     } else {
-      console.log("serving panel from cache");
       panel = panelCache[`${dashboardUid}-${panelId}`];
     }
-    console.log("getPanel. after getDashboardByUid. dash, panel:", dash, panel);
+    if(!panel){ return }
     // this Provider call binds the store in a way that the rest of the app can find it
     return React.createElement(Provider, { store: appContext.store },
       // this one provides a lot of other state informationn and is consistent with
@@ -179,7 +198,7 @@ let getPanel = function(appContext, dashboardUid, panelId, height, width){
           // "lazy" is a very important value here. We probably always
           // want this set up like this in an external panel context.
           React.createElement(DashboardPanel, {
-            stateKey: panel.key,
+            stateKey: panel?.key,
             width: width,
             height: height,
             lazy: false,
