17815a0864530662c8a7c670d376b5b62c35793f
James Kafader <jkafader@es.net>
2025-05-15 14:43:54 -0700

    merge changes from panel-exporter-package

--- packages/grafana-panel-exporter/src/component.js
+++ packages/grafana-panel-exporter/src/component.js
@@ -1,10 +1,5 @@
-// this will be the client side API for the panelExporter
-// it will define a web component and a react component
-// it will export the react component as an ESM and a CJS ES2018 module
-// it will export the web component as an ESM.
-
-export class GrafanaPanelWC extends HTMLElement {
-    static observedAttributes = ["dashboard-uid", "panel-id", "height", "width"];
+export class GrafanaPanelComponent extends HTMLElement {
+    static observedAttributes = ["dashboard-uid", "panel-id", "height", "width", "login-redirect"];
 
     connectedCallback(){
         this.render();
@@ -19,20 +14,19 @@ export class GrafanaPanelWC extends HTMLElement {
         if(!window.Grafana) {
             console.error("window.Grafana not found"); return;
         }
-        if(!window.grafanaBootData?.settings?.datasources){
-            console.error("window.grafanaBootData not found"); return;
-        }
 
-        let style = `height:${this.height+20}px; width:${this.width+20}px;`;
-        this.innerHTML = `<div class="grafana panel exporter" style="${style}"></div>`;
+        let style = `height:${parseInt(this.height)}px; width:${parseInt(this.width)}px;`;
+        this.innerHTML = `<div class="grafana-panel-exporter" style="${style}"></div>`;
 
-        let bindingPoint = this.querySelector(".grafana.panel.exporter");
+        let bindingPoint = this.querySelector(".grafana-panel-exporter");
 
-        window.Grafana.Context(this.props.dashboardUid).then((appContext)=>{
+        let doLoginRedirect = typeof(this['login-redirect']) === "undefined" || this['login-redirect'] === true;
+
+        window.Grafana.Context(this['dashboard-uid'], doLoginRedirect).then((appContext)=>{
             window.Grafana.bindPanelToElement(
                 appContext,
-                this.dashboardUid,
-                this.panelId,
+                this['dashboard-uid'],
+                this['panel-id'],
                 bindingPoint,
                 parseInt(this.height),
                 parseInt(this.width)
@@ -41,4 +35,4 @@ export class GrafanaPanelWC extends HTMLElement {
     }
 }
 
-customElements.define('grafana-panel', GrafanaPanelWC);
+customElements.define('grafana-panel', GrafanaPanelComponent);
